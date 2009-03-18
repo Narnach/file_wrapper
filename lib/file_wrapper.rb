fb5e3980e285cebe50a15984a4ed5c2985cf624a
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2009-03-18 16:31:38 +0100

    FileWrapper can now handle filenames with spaces as it wraps it in double quotes.

--- lib/file_wrapper.rb
+++ lib/file_wrapper.rb
@@ -4,7 +4,7 @@ class FileWrapper
     # Extract the mime type from a file.
     def get_mime(filename)
       raise ArgumentError, "Invalid file" unless File.exist? filename and File.file? filename and File.readable? filename
-       file_output = `file --mime #{filename} 2>&1`.strip
+       file_output = `file --mime "#{filename}" 2>&1`.strip
        matches = file_output.match(/.+?:\s+([\/\d\w-]+\/[\/\d\w-]+)/)
        if matches
          return matches[1]
--- test/file_wrapper_test.rb
+++ test/file_wrapper_test.rb
@@ -1,8 +1,20 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'test/unit'
 require 'file_wrapper'
+require 'tempfile'
 
 class FileWrapperTest < Test::Unit::TestCase
   def test_rakefile_is_text
     assert_equal 'text/plain', FileWrapper.get_mime('Rakefile')
+  end
+
+  def test_filenames_with_spaces
+    Tempfile.open('text file with spaces.txt') do |f|
+      File.open(f.path,'w') do |fp|
+        fp.puts 'test'
+      end
+      assert_equal 'text/plain', FileWrapper.get_mime(f.path)
+    end
   end
 end
